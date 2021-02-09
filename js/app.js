c680ae7a31dc3f45c85a72710db88ef43a82ffb2
davila41 <jose.augustodavilag@gmail.com>
2021-02-09 12:35:50 -0300

    Add animação de entrada

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', () => {
     
     let tl = new TimelineMax();
 
-    
+    tl.fromTo('.bg-loader', 1 ,
+        {width: '100%'},
+     {width: '0%', delay: 5 , ease: Expo.easeInOut})
 
 })
